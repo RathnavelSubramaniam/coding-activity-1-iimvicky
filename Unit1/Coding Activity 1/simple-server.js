62971abd608fac2638a62514b6ee8c52150b5130
iimvicky <vigneshbabuk20092000@gmail.com>
2023-07-10 11:49:33 +0530

    server file created

--- Unit1/Coding Activity 1/simple-server.js
+++ Unit1/Coding Activity 1/simple-server.js
@@ -1,21 +1,12 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import bodyParser from 'body-parser';
-
+const express=require("express");
 const app= express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-app.use(bodyParser.urlencoded({extended:true}));
-const port=3008;
+const port=3009;
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname +"/bmicalculater.html");
+    res.send("Hello World")
 })
-app.post('/',(req,res)=>{
-    var weight=Number(req.body.weight);
-    var height=Number(req.body.height)/100;
-    const result = weight/(height*height);
-    res.send("Your BMI  is " + result.toFixed(2) );
+
+app.get('/data',(req,res)=>{
+    res.send("Hello World with data")
 })
 app.listen(port,()=>{
     console.log(`Example app listening the port ${port}`)
